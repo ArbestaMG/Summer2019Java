2094556484ed6a3d6f8f11e7b1bca061b0d8aa60
Oguz Furkan MAGDEN <55003591+ofmagden@users.noreply.github.com>
2019-11-21 16:55:25 -0500

    Add files via upload

---
+++ Review.java
@@ -0,0 +1,21 @@
+package day06_ArithmaticOperators;
+
+public class Review { 
+ public static void main(String[] args) {
+	int a=10;
+	int b=15;
+	
+	a=a+b; //a=25
+	b=a-b;  //25-15=10; b=10
+	a= a-b; //25-10=15; a=15
+	
+	System.out.println(a+"\n"+b);
+	
+	// burada a=10, b=15. biz bunlarin degerini 3. bi variable kullanmadan degistirmek istedik. o yuzden bunu yaptik.
+	
+	
+	
+	
+}
+
+}
